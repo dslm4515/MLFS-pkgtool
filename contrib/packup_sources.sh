fdc6bcdfaccb17bd4d42ce816bc0e84083986e3e
dslm4515 <27793627+dslm4515@users.noreply.github.com>
2021-06-19 02:24:22 -0500

    Added bootscripts to pack, makes it easier to copy.

--- contrib/packup_sources.sh
+++ contrib/packup_sources.sh
@@ -6,3 +6,7 @@ tar Jcf /tmp/tar-1.13.tar.xz tar-1.13
 echo "Packing up Bash Startup scripts"
 tar Jcpf /tmp/mlfs-bash_startup_scripts-1.00.tar.xz \
          mlfs-bash_startup_scripts-1.00
+
+echo "packing up bootscripts" &&
+tar Jcpf /tmp/MLFS-S6-Bootscripts.tar.xz \
+         MLFS-S6-Bootscripts
